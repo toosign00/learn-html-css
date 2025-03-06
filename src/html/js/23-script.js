4aaabb5dc6206c76358d1a8ab89b9c4c00e32e76
toosign <kevinsoo1014@gmail.com>
2025-03-06 16:08:30 +0900

    src pull

--- src/html/js/23-script.js
+++ src/html/js/23-script.js
@@ -1,8 +1,3 @@
-const flipButton = document.querySelector('.flip-button');
-const card = document.querySelector('.card');
-
-const flipCard = () => {
-  card.classList.toggle('is-flipped');
-};
-
-flipButton.addEventListener('click', flipCard);
+document.querySelector('.flip-button').addEventListener('click', () => {
+  document.querySelector('.card').classList.toggle('is-flipped');
+});
